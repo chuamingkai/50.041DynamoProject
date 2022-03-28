c89c8050626f76744ddbe1339014506f353c81ff
carrot <caryl830@gmail.com>
2022-03-28 22:05:45 +0800

    Add physical clock timestamp to Object struct

--- internal/models/object.go
+++ internal/models/object.go
@@ -1,7 +1,14 @@
 package models
 
+import (
+	"time"
+)
+
+// TODO: Add vector clock functions
 type Object struct {
 	Key string `json:"key"`
 	Value string `json:"value"`
-	VC     map[string]uint64 `json:",omitempty"`
+	VC     map[string]uint64 `json:"VC"`
+	CreatedOn time.Time `json:",omitempty"`
+	LastModifiedOn time.Time `json:",omitempty"`
 }
--- internal/nodes/repserver.go
+++ internal/nodes/repserver.go
@@ -40,23 +40,22 @@ type PutRepResult struct {
 	Err error `json:"error"`
 }
 
-func (s *nodesServer) serverGetReplicas(ctx context.Context, req *pb.GetRepRequest) (*GetRepResult, error) {
-	log.Printf("Received GET request from clients for key %s\n", req.Key)
 
+func (s *nodesServer) serverGetReplicas(bucketName, key string) (*GetRepResult, error) {
 	// Get preference list for the key
-	prefList := s.ring.GetPreferenceList(req.Key)
+	prefList := s.ring.GetPreferenceList(key)
 
 	// Create context to get replicas
-	clientDeadline := time.Now().Add(5 * time.Second)
-	ctxRep, cancel := context.WithDeadline(ctx, clientDeadline)
+	serverDeadline := time.Now().Add(5 * time.Second)
+	ctxRep, cancel := context.WithDeadline(context.Background(), serverDeadline)
 	defer cancel()
 
 	// Send GetRep request to all servers in preference list
 	notifyChan := make(chan GetRepMessage, consistenthash.REPLICATION_FACTOR)
 	for _, virtualNode := range prefList {
 		getRepReq := pb.GetRepRequest{
-			BucketName: req.BucketName,
-			Key: req.Key,
+			BucketName: bucketName,
+			Key: key,
 		}
 
 		// Send GetRep request to virtual node
@@ -81,11 +80,6 @@ func (s *nodesServer) serverGetReplicas(ctx context.Context, req *pb.GetRepReque
 					}
 				} else {
 					log.Printf("Error contacting node %v: %v\n", vn.NodeId, err)
-					// notifyChan <- GetRepMessage{
-					// 	peerId: vn.NodeId,
-					// 	err: err,
-					// }
-					// return
 				}
 				defer conn.Close()
 			}
@@ -130,15 +124,15 @@ func (s *nodesServer) serverGetReplicas(ctx context.Context, req *pb.GetRepReque
 		select {
 		case notifyMsg := <- notifyChan:
 			if notifyMsg.err == nil {
-				log.Printf("Received GET replica response from node %v for key {%v}: val {%v}", notifyMsg.peerId, req.Key, notifyMsg.repObject)
+				log.Printf("Received GET replica response from node %v for key {%v}: val {%v}", notifyMsg.peerId, key, notifyMsg.repObject.Value)
 				successCount++
 				replicas = append(replicas, notifyMsg.repObject)
 			} else if e, ok := status.FromError(notifyMsg.err); ok && e.Code() == codes.NotFound {
-				log.Printf("Received GET replica response from node %v for key {%v}: key not found", notifyMsg.peerId, req.Key)
+				log.Printf("Received GET replica response from node %v for key {%v}: key not found", notifyMsg.peerId, key)
 				successCount++
 			} else {
 				errorMsg = errorMsg + notifyMsg.err.Error() + ";"
-				log.Printf("Received GET replica response from node %v for key {%v}: Error occurred: %v", notifyMsg.peerId, req.Key, notifyMsg.err.Error())
+				log.Printf("Received GET replica response from node %v for key {%v}: Error occurred: %v", notifyMsg.peerId, key, notifyMsg.err.Error())
 			}
 		case <- ctxRep.Done():
 		}
@@ -147,9 +141,8 @@ func (s *nodesServer) serverGetReplicas(ctx context.Context, req *pb.GetRepReque
 		}
 	}
 
-	log.Printf("Finished getting replicas for key %v, received %v successful response(s).\n", req.Key, successCount)
+	log.Printf("Finished getting replicas for key %v, received %v successful response(s).\n", key, successCount)
 
-		
 	if successCount == 0 { // No responses received -> servers in preference list are down
 		return nil, errors.New(errorMsg)
 	} else if len(replicas) == 0 { // No replicas received -> key does not exist in database
@@ -161,27 +154,22 @@ func (s *nodesServer) serverGetReplicas(ctx context.Context, req *pb.GetRepReque
 	}
 }
 
-func (s *nodesServer) serverPutReplicas(ctx context.Context, req *pb.PutRepRequest) *PutRepResult {
-	var reqObject models.Object
-	if err := json.Unmarshal(req.Data, &reqObject); err != nil{
-		return &PutRepResult{Success: false, Err: err}
-	}
-	log.Printf("Received PUT request from clients for key %s, value %v\n", req.Key, reqObject)
-
+func (s *nodesServer) serverPutReplicas(bucketName, key string, data []byte) *PutRepResult {
 	// Get preference list for the key
-	prefList := s.ring.GetPreferenceList(req.Key)
+	prefList := s.ring.GetPreferenceList(key)
 
 	// Create context to get replicas
-	clientDeadline := time.Now().Add(5 * time.Second)
-	ctxRep, cancel := context.WithDeadline(ctx, clientDeadline)
+	serverDeadline := time.Now().Add(10 * time.Second)
+	ctxRep, cancel := context.WithDeadline(context.Background(), serverDeadline)
 	defer cancel()
 
 	// Send GetRep request to all servers in preference list
 	notifyChan := make(chan PutRepMessage, consistenthash.REPLICATION_FACTOR)	
 	putRepReq := &pb.PutRepRequest{
-		Key: req.Key,
-		Data: req.Data,
-		BucketName: req.BucketName,
+		Key: key,
+		Data: data,
+		BucketName: bucketName,
+		SenderId: s.nodeId,
 	}
 	for _, virtualNode := range prefList {
 		var putRepRes *pb.PutRepResponse
@@ -206,10 +194,19 @@ func (s *nodesServer) serverPutReplicas(ctx context.Context, req *pb.PutRepReque
 				}
 				defer conn.Close()
 			}
-			notifyChan <- PutRepMessage{
-				peerId: vn.NodeId,
-				putSuccess: putRepRes.IsDone,
-				err: err,
+
+			if err != nil {
+				notifyChan <- PutRepMessage{
+					peerId: vn.NodeId,
+					putSuccess: false,
+					err: err,
+				}
+			} else {
+				notifyChan <- PutRepMessage{
+					peerId: vn.NodeId,
+					putSuccess: putRepRes.IsDone,
+					err: nil,
+				}
 			}
 		}(virtualNode, notifyChan)
 	}
@@ -220,11 +217,11 @@ func (s *nodesServer) serverPutReplicas(ctx context.Context, req *pb.PutRepReque
 		select {
 		case notifyMsg := <- notifyChan:
 			if notifyMsg.err == nil {
-				log.Printf("Received PUT replica response from server %v for key {%v} val {%v}: SUCCESS", notifyMsg.peerId, req.Key, reqObject)
+				log.Printf("Received PUT replica response from server %v for key {%v} val {%s}: SUCCESS", notifyMsg.peerId, key, data)
 				successCount++
 			} else {
 				errorMsg = errorMsg + notifyMsg.err.Error() + ";"
-				log.Printf("Received PUT replica response from server %v for key {%v} val {%v}: ERROR occured: %v\n", notifyMsg.peerId, req.Key, reqObject, notifyMsg.err.Error())
+				log.Printf("Received PUT replica response from server %v for key {%v} val {%s}: ERROR occured: %v\n", notifyMsg.peerId, key, data, notifyMsg.err.Error())
 			}
 		case <- ctxRep.Done():
 		}
@@ -233,14 +230,14 @@ func (s *nodesServer) serverPutReplicas(ctx context.Context, req *pb.PutRepReque
 		}
 	}
 
-	log.Printf("Finished PUTTING replica for key {%v} val {%v}, received {%v} response", req.Key, reqObject, successCount)
+	log.Printf("Finished PUTTING replica(s) for key {%v}, received {%v} response", key, successCount)
 	if successCount == 0 { // All servers failed to save replica
 		return &PutRepResult{Success: false, Err: errors.New(errorMsg)}
 	} else {
 		// TODO: Add case to handle partial success (not all servers in preference list successfuly saved replica)
 		return &PutRepResult{Success: true, Err: nil}
 	}
-} 
+}
 
 func allSame(replicas []models.Object) bool {
 	for i := 1; i < len(replicas); i++ {
