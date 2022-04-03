22079dd6b1309595d2f68c04ea70f2a753d64439
chuamingkai <chuamingkai@gmail.com>
2022-04-03 20:52:49 +0800

    more config vars

--- config/config.go
+++ config/config.go
@@ -1,6 +1,12 @@
 package config
 
+import "time"
+
 const (
-	NUM_VIRTUAL_NODES  int = 3
-	REPLICATION_FACTOR int = 3 // N value
+	NUM_VIRTUAL_NODES      int           = 3
+	REPLICATION_FACTOR     int           = 3                      // N value
+	RING_BACKUP_FILEFORMAT string        = "store/node%dring.bak" // %d format is compulsory
+	RING_BACKUP_INTERVAL   time.Duration = 1 * time.Minute
+	HINT_CHECK_INTERVAL    time.Duration = 5 * time.Minute
+	HINT_BUCKETNAME        string        = "hints"
 )
