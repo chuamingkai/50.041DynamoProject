f85a88a77cd7ec3aefd4de9f21b63c7e7256c258
Cassie Ang <cassie_ang@mymail.sutd.edu.sg>
2022-04-04 11:35:51 +0800

    updated node handlers (load balancer not complete)

--- cmd/nodemanager/main.go
+++ cmd/nodemanager/main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strconv"
+	"encoding/binary"
 
 	"github.com/gorilla/mux"
 
@@ -22,12 +26,9 @@ var ClientReq []ClientRequest
 var Nodes []UpdateNode //not displayed
 
 type NodeManager struct {
-	RingList consistenthash.Ring
+	nodeID   int64
+	RingList *consistenthash.Ring
 }
-
-var manager NodeManager
-
-// object to send to nodes:
 
 // ClientHTPP e.g.:{"key":"A1234"}
 // format for client to request for a node using key on GET
@@ -35,11 +36,13 @@ type ClientRequest struct {
 	Key string `json:"key"`
 }
 
+// object to send to nodes:
 type UpdateNode struct {
 	Key    string `json:"key"`
 	PortNo uint64 `json:"portno"`
 }
 
+// port 8000 home page display
 func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Node manager landing @ 8000!\n")
 	fmt.Fprintf(w, "For clients, make a POST request to '/client' with a key. The manager will return the node you will need to communicate with.\n")
@@ -48,23 +51,25 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Endpoint Hit: nodemanager http server")
 }
 
-func handleRequests() {
+func (m *NodeManager) handleRequests()  {
+
+	fmt.Println("Node manager started at 8000!")
 	myRouter := mux.NewRouter().StrictSlash(true)
 	myRouter.HandleFunc("/", homePage)
-	myRouter.HandleFunc("/allClient", returnAllClientReq)      // all client reqs printed to localhost
-	myRouter.HandleFunc("/client", newRequest).Methods("POST") //allow client to post to manager to get node portno
+	myRouter.HandleFunc("/allClient", returnAllClientReq)
+	myRouter.HandleFunc("/client", m.newRequest).Methods("POST") //allow client to post to manager to get node portno
 	myRouter.HandleFunc("/client/{key}", returnSingleKey)
 	//myRouter.HandleFunc("/allNodes", returnAllNodes)            // all existing nodes -- doesnt work yet
-	myRouter.HandleFunc("/addNode", addNodeReq).Methods("POST") // managing ADD
-	myRouter.HandleFunc("/delNode", delNodeReq).Methods("POST") // managing DEL
+	myRouter.HandleFunc("/addNode", m.addNodeReq).Methods("POST") // managing ADD
+	myRouter.HandleFunc("/delNode", m.delNodeReq).Methods("POST") // managing DEL
 	//myRouter.HandleFunc("/delNode", deleteKey).Methods("POST")  // testing local array delete
+
 	log.Fatal(http.ListenAndServe(":8000", myRouter))
 }
 
-func addNodeReq(w http.ResponseWriter, r *http.Request) {
-	// get the body of our POST request
-	// unmarshal this into a new ClientRequest struct
-	// append this to our ClientReq array, and check for node responsible for key
+// respond to the requesting add node with the current ring
+func (m *NodeManager) addNodeReq(w http.ResponseWriter, r *http.Request) {
+
 	reqBody, _ := ioutil.ReadAll(r.Body)
 	var article UpdateNode
 
@@ -72,28 +77,110 @@ func addNodeReq(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		panic(err)
 	}
-	Nodes = append(Nodes, article)
+	//Nodes = append(Nodes, article)
 	json.NewEncoder(w).Encode(&article)
 	// add new node to the ring
 	fmt.Println("Adding node", article.PortNo, " to existing ring.")
-	manager.addNode(article.Key, article.PortNo)
+	m.addNode(article.Key, article.PortNo)
+
+	exist := false
+	var newnode UpdateNode
+	for _, n := range m.RingList.NodeMap {
+		fmt.Println(n.NodeId, newnode.PortNo)
+		if n.NodeId == newnode.PortNo {
+			exist = true
+		}
+	}
+	// Make request to nodes to addnode
+	if !exist {
+		if len(m.RingList.NodeMap) > 1 {
+			for _, node := range m.RingList.NodeMap {
+				fmt.Println("Requesting to add node to", article.PortNo)
+				postBody, _ := json.Marshal(newnode)
+				responseBody := bytes.NewBuffer(postBody)
+				resp, err := http.Post(fmt.Sprintf("http://localhost:%v/addnode", node.NodeId), "application/json", responseBody)
+				if err != nil {
+					// handle error
+					continue
+				}
+				// close response body when finished with it
+				defer resp.Body.Close()
+				body, err := io.ReadAll(resp.Body)
+				if err != nil {
+					continue
+				}
+				// this body is unreadable/may need another conversion first. Using simple string(body) causes a parsing error so this is roundabout
+				strBody := strconv.FormatUint(binary.LittleEndian.Uint64(body),16)
+				log.Println(strBody)
+				//fmt.Println("Response body addNode",string(body))
+			}
+		}
+		json.NewEncoder(w).Encode(m.RingList.Nodes.TraverseAndPrint())
+	} else {
+		fmt.Println("Failed to add a node: already exists.")
+		http.Error(w, "Node already exists!", http.StatusBadRequest)
+	}
 }
 
-func delNodeReq(w http.ResponseWriter, r *http.Request) {
+func (m *NodeManager) delNodeReq(w http.ResponseWriter, r *http.Request) {
 	// get the body of our POST request
 	// unmarshal this into a new ClientRequest struct
 	// append this to our ClientReq array, and check for node responsible for key
 	reqBody, _ := ioutil.ReadAll(r.Body)
-	var article UpdateNode
+	var delNode UpdateNode
 
-	var err error = json.Unmarshal(reqBody, &article)
+	var err error = json.Unmarshal(reqBody, &delNode)
 	if err != nil {
 		panic(err)
 	}
 
-	// add new node to the ring
-	fmt.Println("Removing node", article.Key, " from existing ring.")
-	manager.removeNode(article.Key, article.PortNo)
+	// del node from the ring
+	fmt.Println("Removing node", delNode.Key, " from ring.")
+	//manager.removeNode(delNode.Key, delNode.PortNo)
+
+	exist := false
+	for _, n := range m.RingList.NodeMap {
+		if n.NodeId == delNode.PortNo {
+			exist = true
+		}
+	}
+	if exist {
+		m.RingList.RemoveNode(delNode.Key)
+
+		/*send http post request to all nodes it knows*/
+		if m.RingList.Nodes.Length > 1 {
+
+			for _, node := range m.RingList.NodeMap {
+
+				postBody, _ := json.Marshal(delNode)
+				responseBody := bytes.NewBuffer(postBody)
+				// make Req to nodes to delete
+				resp, err := http.Post(fmt.Sprintf("http://localhost:%v/delnode", node.NodeId), "application/json", responseBody)
+				//Handle Error
+				if err != nil {
+					log.Fatalf("An Error Occured %v", err)
+				}
+				// close response body when finished with it
+				defer resp.Body.Close()
+				//Read the response body
+				body, err := ioutil.ReadAll(resp.Body)
+				if err != nil {
+					continue
+				}
+				// this body is unreadable/may need another conversion first. Using simple string(body) causes a parsing error so this is roundabout
+				strBody := strconv.FormatUint(binary.LittleEndian.Uint64(body),16)
+				log.Println(strBody)
+				//fmt.Println("Response body addNode",string(body))
+
+			}
+		}
+
+		fmt.Println(m.RingList.Nodes.TraverseAndPrint())
+	} else {
+		fmt.Println("Failed to delete node: node doesn't exist.")
+		http.Error(w, "Node doesn't exist!", http.StatusServiceUnavailable)
+	}
+	json.NewEncoder(w).Encode(delNode)
 }
 
 func returnSingleKey(w http.ResponseWriter, r *http.Request) {
@@ -108,18 +195,19 @@ func returnSingleKey(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// unused - to display all currently listed nodes on manager
 func returnAllNodes(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Endpoint Hit: returnAllNodes")
 	json.NewEncoder(w).Encode(Nodes)
 }
 
-// encode keys to arr->json str
+// displays all client reqs to date
 func returnAllClientReq(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Endpoint Hit: returnAllClientReq")
 	json.NewEncoder(w).Encode(ClientReq)
 }
 
-func newRequest(w http.ResponseWriter, r *http.Request) {
+func (m *NodeManager) newRequest(w http.ResponseWriter, r *http.Request) {
 	// get the body of our POST request
 	// unmarshal this into a new ClientRequest struct
 	// append this to our ClientReq array, and check for node responsible for key
@@ -139,7 +227,7 @@ func newRequest(w http.ResponseWriter, r *http.Request) {
 
 	// search for expected node on pref list
 	fmt.Println("Searching for node for requested key", article.Key)
-	var response = manager.RingList.GetPreferenceList(article.Key)
+	var response = m.RingList.GetPreferenceList(article.Key)
 
 	// since we are simply emulating client, print response to terminal
 	fmt.Println("The node responsible is at Node ID:", response[0].NodeId, "\n================================================================")
@@ -164,13 +252,13 @@ func deleteKey(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 }
-
+/*
 func (m *NodeManager) removeNode(name string, id uint64) {
 	fmt.Println("Removing node", name, " at port:", id)
-	m.RingList.RemoveNode(name)
+	//m.RingList.RemoveNode(name)
 	fmt.Print("Deleted! Current nodes: ")
-	fmt.Println(m.RingList.Nodes.TraverseAndPrint())
-}
+	//fmt.Println(m.RingList.Nodes.TraverseAndPrint())
+}*/
 
 func (m *NodeManager) addNode(name string, id uint64) {
 	fmt.Println("Adding node", name, " at port:", id)
@@ -180,65 +268,32 @@ func (m *NodeManager) addNode(name string, id uint64) {
 	fmt.Println("\n================================================================")
 }
 
-func initManager() {
-	manager.RingList = *consistenthash.NewRing()
+func initManager(port int64, newRing *consistenthash.Ring) *NodeManager {
+	return &NodeManager{
+		nodeID:   port,
+		RingList: newRing,
+	}
 }
 
 func main() {
 
-	nodeNames := []string{"1", "2", "3", "4"}
-	nodeNumbers := []uint64{9030, 9040, 9050, 9060}
-	initManager()
-	fmt.Println("Manager running at 8000! ")
+	//nodeNames := []string{"1", "2", "3", "4"}
+	//nodeNumbers := []uint64{9030, 9040, 9050, 9060}
 
-	testManager(nodeNames, nodeNumbers)
+	managerRing := consistenthash.NewRing()
+	m := initManager(8000, managerRing)
+
+	//testManager(nodeNames, nodeNumbers)
 	//populate with dummy data
 	//ClientReq = []ClientRequest{}
+	m.handleRequests()
 
-	handleRequests()
 }
-
-func testManager(nodenames []string, nodeIDs []uint64) {
+/*
+func (m *NodeManager) testManager(nodenames []string, nodeIDs []uint64) {
 	for i := 0; i < len(nodenames); i++ {
-		manager.addNode(nodenames[i], nodeIDs[i])
+		m.addNode(nodenames[i], nodeIDs[i])
 		fmt.Println("Returned from adding node ", nodeIDs[i], " to local list.")
 	}
 }
-
-/*
-	// id := 55 // TODO: Dynamically assign node IDs
-	testEntry := models.Object{
-        Key string `json:"key"`
-	    Value string `json:"value"`
-	    VC     map[string]uint64 `json:",omitempty"`
-		//IC: "S1234567A",
-		//GeoLoc: "23:23:23:23 NW",
-	}
-
-	// Open database
-	db, err := bolt.ConnectDB(id)
-	if err != nil {
-		log.Fatalf("Error opening database: %s", err)
-	}
-	defer db.DB.Close()
-
-	// Create bucket
-	err = db.CreateBucket("testBucket")
-	if err != nil {
-		log.Fatalf("Error creating bucket: %s", err)
-	}
-
-	// Insert test value into bucket
-	err = db.Put("testBucket", testEntry)
-	if err != nil {
-		log.Fatalf("Error inserting into bucket: %s", err)
-	}
-
-	// Read from bucket
-	value := db.Get("testBucket", testEntry.IC)
-	fmt.Printf("Value at key %s: %s", testEntry.IC, value)
-}
-	// getResult := db.Get("testBucket", testEntry.Key)
-	// fmt.Println(getResult)
-
 */
